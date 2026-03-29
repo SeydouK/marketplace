9414944ddde18bbc13f482e54e3cc951ebdaf794
koffi ali <kali003@enseirb-matmeca.fr>
2026-03-29 19:08:58 +0000

    Refactoring de Laravel à Angular spring

--- resources/views/components/primary-button.blade.php
+++
@@ -1,3 +0,0 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-800 focus:bg-gray-900 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 bg-red-600']) }}>
-    {{ $slot }}
-</button>
